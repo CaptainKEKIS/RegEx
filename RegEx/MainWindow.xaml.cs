2d27ad579e1349033d0bec0fa39ff61a0c223565
KustDV <KustDV@ZLOY>
2019-02-11 11:35:31 +0700

    2 zadaniya

--- RegEx/MainWindow.xaml.cs
+++ RegEx/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,32 @@ namespace RegEx
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Regex r1 = new Regex(@"^a$|^aaaaaa$|^a\saa\sa$");
+            if (r1.IsMatch(TBOdin.Text))
+            {
+                MessageBox.Show("OK!");
+            }
+            else
+            {
+                MessageBox.Show("Not OK!");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Regex r1 = new Regex(@"^\w{5,}$");
+            if (r1.IsMatch(TBDva.Text))
+            {
+                MessageBox.Show("OK!");
+            }
+            else
+            {
+                MessageBox.Show("Not OK!");
+            }
         }
     }
 }
